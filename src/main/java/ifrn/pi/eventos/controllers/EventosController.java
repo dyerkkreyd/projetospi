4dbabf743e63d57764949e5faa64731696296a42
dyerkkreyd <nandinhovictor78@gmail.com>
2022-08-31 14:19:24 -0300

    Criando página submit

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -8,6 +8,15 @@ public class EventosController {
 	
 	@RequestMapping("/eventos/form")
 	public String form() {
+		System.out.println("Chamou o método form!");
 		return "formEvento";
+	}
+	@RequestMapping("/eventos/submit")
+	public String submit(String nome, String data, String local, String horario)  {
+		System.out.println("nome");
+		System.out.println("data");
+		System.out.println("local");
+		System.out.println("horario");
+		return "submit";
 	}
 }
