8bf79bcd8e84600bac531f8945085f215685f6e5
dyerkkreyd <nandinhovictor78@gmail.com>
2022-10-25 13:51:06 -0300

    Resolvendo erros simples

--- src/main/java/ifrn/eventos/controllers/IndexController.java
+++ src/main/java/ifrn/eventos/controllers/IndexController.java
@@ -8,7 +8,7 @@ public class IndexController {
 
 	@RequestMapping("/")
 	public String index() {
-		System.out.println("Chamou o método index");
+		System.out.println("Redirecinando...");
 		return "redirect:/eventos";
 	}
 	
